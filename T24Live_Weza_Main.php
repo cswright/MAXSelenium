979f5969967668b76066e19b624179fe5edea534
Clinton Wright <cwright@bwtsgroup.com>
2014-07-16 16:10:35 +0200

    Updated commit of starting T24 Weza main script

--- T24Live_Weza_Main.php
+++ T24Live_Weza_Main.php
@@ -52,6 +52,7 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 	protected $_mode;
 	protected $_ip;
 	protected $_xls;
+	protected $_truckDescription;
 	protected $_wdport;
 	protected $_proxyip;
 	protected $_username;
@@ -74,7 +75,14 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 	);
 	
 	// : Public Functions
+	
 	// : Accessors
+	// : End
+	
+	// : Setters
+	// : End
+	
+	// : Getters
 	// : End
 	
 	// : Magic
@@ -89,7 +97,7 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 			return FALSE;
 		}
 		$data = parse_ini_file ( $ini );
-		if ((array_key_exists ( "screenshotdir", $data ) && $data ["screenshotdir"]) && (array_key_exists ( "errordir", $data ) && $data ["errordir"]) && (array_key_exists ( "xls", $data ) && $data ["xls"]) && (array_key_exists ( "wdport", $data ) && $data ["wdport"]) && (array_key_exists ( "datadir", $data ) && $data ["datadir"]) && (array_key_exists ( "ip", $data ) && $data ["ip"]) && (array_key_exists ( "username", $data ) && $data ["username"]) && (array_key_exists ( "password", $data ) && $data ["password"]) && (array_key_exists ( "welcome", $data ) && $data ["welcome"]) && (array_key_exists ( "mode", $data ) && $data ["mode"])) {
+		if ((array_key_exists ( "screenshotdir", $data ) && $data ["screenshotdir"]) && (array_key_exists ( "errordir", $data ) && $data ["errordir"]) && (array_key_exists ( "xls", $data ) && $data ["xls"]) && (array_key_exists ( "wdport", $data ) && $data ["wdport"]) && (array_key_exists ( "datadir", $data ) && $data ["datadir"]) && (array_key_exists ( "ip", $data ) && $data ["ip"]) && (array_key_exists ( "username", $data ) && $data ["username"]) && (array_key_exists ( "password", $data ) && $data ["password"]) && (array_key_exists ( "welcome", $data ) && $data ["welcome"]) && (array_key_exists ( "mode", $data ) && $data ["mode"]) && (array_key_exists ( "truckDescription", $data ) && $data ["truckDescription"])) {
 			$this->_username = $data ["username"];
 			$this->_password = $data ["password"];
 			$this->_welcome = $data ["welcome"];
@@ -102,6 +110,7 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 			$this->_proxyip = $data["proxy"];
 			$this->_browser = $data ["browser"];
 			$this->_xls = $data ["xls"];
+			$this->_truckDescription = $data["truckDescription"];
 			switch ($this->_mode) {
 				case "live" :
 					$this->_maxurl = self::LIVE_URL;
@@ -134,23 +143,7 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 		$this->_session = self::$driver->session ( $this->_browser, $desired_capabilities );
 	}
 	
-	// : Setters
-	public function SetcURLTimeout($_Timeout) {
-		if (is_int ( $_Timeout )) {
-			$this->_cURLTimeout = $_Timeout;
-			return TRUE;
-		} else {
-			return FALSE;
-		}
-	}
-	// : End
-	
-	// : Getters
-	public function GetcURLTimeout() {
-		return $this->_cURLTimeout;
-	}
-	
-	// : End
+	// : Selenium Test Functions
 	
 	/**
 	 * T24Live_Weza_Main::testFunctionTemplate
@@ -262,107 +255,36 @@ class T24Live_Weza_Main extends PHPUnit_Framework_TestCase {
 		// : End
 	}
 	
+	// : End
+	
 	// : Private Functions
+	/**
+	 * T24Live_Weza_Main::writeCSVFile($_data, $_file)
+	 * Write CSV File of given array
+	 *
+	 * @param array: $_data
+	 * @param string: $_file
+	 */
+	private function writeCSVFile($_data, $_file) {
+		
+	}
 	
 	/**
-	 * T24Live_Weza_Main::writeExcelFile($excelFile, $excelData)
-	 * Create, Write and Save Excel Spreadsheet from collected data obtained from the variance report
+	 * T24Live_Weza_Main::readCSVFile($_file)
+	 * Read CSV File of given array
 	 *
-	 * @param $excelFile, $excelData        	
+	 * @param array: $_data
+	 * @param string: $_file
 	 */
-	public function writeExcelFile($excelFile, $excelData, $columns) {
-		try {
-			// Check data validility
-			if (count ( $excelData ) != 0) {
-				
-				// : Create new PHPExcel object
-				print ("<pre>") ;
-				print (date ( 'H:i:s' ) . " Create new PHPExcel object" . PHP_EOL) ;
-				$objPHPExcel = new PHPExcel ();
-				// : End
-				
-				// : Set properties
-				print (date ( 'H:i:s' ) . " Set properties" . PHP_EOL) ;
-				$objPHPExcel->getProperties ()->setCreator ( self::XLS_CREATOR );
-				$objPHPExcel->getProperties ()->setLastModifiedBy ( self::XLS_CREATOR );
-				$objPHPExcel->getProperties ()->setTitle ( self::XLS_TITLE );
-				$objPHPExcel->getProperties ()->setSubject ( self::XLS_SUBJECT );
-				// : End
-				
-				// : Setup Workbook Preferences
-				print (date ( 'H:i:s' ) . " Setup workbook preferences" . PHP_EOL) ;
-				$objPHPExcel->getDefaultStyle ()->getFont ()->setName ( 'Arial' );
-				$objPHPExcel->getDefaultStyle ()->getFont ()->setSize ( 8 );
-				$objPHPExcel->getActiveSheet ()->getPageSetup ()->setOrientation ( PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE );
-				$objPHPExcel->getActiveSheet ()->getPageSetup ()->setPaperSize ( PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4 );
-				$objPHPExcel->getActiveSheet ()->getPageSetup ()->setFitToWidth ( 1 );
-				$objPHPExcel->getActiveSheet ()->getPageSetup ()->setFitToHeight ( 0 );
-				// : End
-				
-				// : Set Column Headers
-				$alphaVar = range ( 'A', 'Z' );
-				print (date ( 'H:i:s' ) . " Setup column headers" . PHP_EOL) ;
-				
-				$i = 0;
-				foreach ( $columns as $key ) {
-					$objPHPExcel->getActiveSheet ()->setCellValue ( $alphaVar [$i] . "1", $key );
-					$objPHPExcel->getActiveSheet ()->getStyle ( $alphaVar [$i] . '1' )->getFont ()->setBold ( true );
-					$i ++;
-				}
-				
-				// : End
-				
-				// : Add data from $excelData array
-				print (date ( 'H:i:s' ) . " Add data from error array" . PHP_EOL) ;
-				$rowCount = ( int ) 2;
-				$objPHPExcel->setActiveSheetIndex ( 0 );
-				foreach ( $excelData as $values ) {
-					$i = 0;
-					foreach ( $values as $key => $value ) {
-						$objPHPExcel->getActiveSheet ()->getCell ( $alphaVar [$i] . strval ( $rowCount ) )->setValueExplicit ( $value, PHPExcel_Cell_DataType::TYPE_STRING );
-						$i ++;
-					}
-					$rowCount ++;
-				}
-				// : End
-				
-				// : Setup Column Widths
-				for($i = 0; $i <= count ( $columns ); $i ++) {
-					$objPHPExcel->getActiveSheet ()->getColumnDimension ( $alphaVar [$i] )->setAutoSize ( true );
-				}
-				// : End
-				
-				// : Rename sheet
-				print (date ( 'H:i:s' ) . " Rename sheet" . PHP_EOL) ;
-				$objPHPExcel->getActiveSheet ()->setTitle ( self::XLS_TITLE );
-				// : End
-				
-				// : Save spreadsheet to Excel 2007 file format
-				print (date ( 'H:i:s' ) . " Write to Excel2007 format" . PHP_EOL) ;
-				print ("</pre>" . PHP_EOL) ;
-				$objWriter = new PHPExcel_Writer_Excel2007 ( $objPHPExcel );
-				$objWriter->save ( $excelFile );
-				$objPHPExcel->disconnectWorksheets ();
-				unset ( $objPHPExcel );
-				unset ( $objWriter );
-				// : End
-			} else {
-				print ("<pre>") ;
-				print_r ( "ERROR: The function was passed an empty array" );
-				print ("</pre>") ;
-				exit ();
-			}
-		} catch ( Exception $e ) {
-			echo "Caught exception: ", $e->getMessage (), "\n";
-			exit ();
-		}
+	private function readCSVFile($_data, $_file) {
+		
 	}
 	
 	/**
 	 * T24Live_Weza_Main::takeScreenshot()
 	 * This is a function description for a selenium test function
 	 *
-	 * @param object: $_session        	
+	 * @param object: $_file        	
 	 */
 	private function takeScreenshot() {
 		$_img = $this->_session->screenshot ();
